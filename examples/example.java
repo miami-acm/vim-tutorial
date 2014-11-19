5a1907950187d1b48a696aa5cb9d96e7e2129222
Nate Mara <natemara@gmail.com>
2014-11-19 18:28:25 -0500

    fix example

--- examples/example.java
+++ examples/example.java
@@ -1,10 +1,10 @@
 public class example {
 	public static void main(String[] args) {
-		System.out.println('Hello world')
+	System.out.println("Hello world");
 
-		if (1+1 == 2) {
-			System.out.println('1+1 is 2!')
-		}
+	if (1+1 == 2) {
+		System.out.println("1+1 is 2!");
+	}
 
 	}
 }
